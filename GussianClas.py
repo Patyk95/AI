f49ced817ba661ca5c665ba13cb213dbe97f1420
Patryk Rajtarowski <p.rajtarowski@gmail.com>
2022-10-05 17:54:08 +0200

    -

--- GussianClas.py
+++ GussianClas.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pandas import array
 import sklearn.datasets
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_absolute_error,median_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
@@ -9,16 +9,18 @@ from sklearn.naive_bayes import GaussianNB
 data= sklearn.datasets.load_diabetes()
 x=(data['data'])
 y=(data['target'])
-print(x.shape)
-print(y.shape)
+#print(x.shape)
+#print(y.shape)
 
 xtrain,xtest,ytrain,ytest= train_test_split(x,y,train_size=0.4)
-print(xtest.shape)
-print(ytest.shape)
+#print(xtest.shape)
+#print(ytest.shape)
 model=GaussianNB()
 model.fit(xtrain,ytrain)
 pred=model.predict(xtest)
-print(pred)
+#print(pred)
 
 print(accuracy_score(ytest,pred))
-print(ytest)
+print(mean_absolute_error(ytest,pred))
+print(median_absolute_error(ytest,pred))
+#print(ytest)
---
+++ LogisticRegresion.py
@@ -0,0 +1,3 @@
+import sklearn.datasets
+import numpy as np
+from sklearn.metrics import accuracy_score
