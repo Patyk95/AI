d3a057ce96e7f4fdee9cebd58c3e982904db5f46
Patryk Rajtarowski <p.rajtarowski@gmail.com>
2022-10-05 18:08:01 +0200

    LogistickRegresion finally version

--- LogisticRegresion.py
+++ LogisticRegresion.py
@@ -1,4 +1,27 @@
 import sklearn.datasets
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn import L
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+model=sklearn.datasets.load_iris()
+
+
+x=model['data']
+y=model['target']
+
+#print(x)
+#print(y)
+
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,train_size=0.35)
+
+clasif=LogisticRegression()
+clasif.fit(xtrain,ytrain)
+pred =clasif.predict(xtest)
+
+print(pred)
+print('efectywne:' ,ytest)
+
+print(accuracy_score(ytest,pred))
